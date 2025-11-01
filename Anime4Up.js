6ef38a3a9ee7c1a57f95e36ddc52e8b69467f170
MxS3 <elsanea2019@gmail.com>
2025-11-01 19:41:37 +0200

    Implement decodeHTMLEntities function
    Added a function to decode HTML entities in text.

--- Anime4Up.js
+++ Anime4Up.js
@@ -349,3 +349,18 @@ async function extractStreamUrl(url) {
     return JSON.stringify({ streams: [] });
   }
 }
+
+function decodeHTMLEntities(text) {
+  text = text.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec));
+  const entities = {
+    '&quot;': '"',
+    '&amp;': '&',
+    '&apos;': "'",
+    '&lt;': '<',
+    '&gt;': '>'
+  };
+  for (const entity in entities) {
+    text = text.replace(new RegExp(entity, 'g'), entities[entity]);
+  }
+  return text;
+}
